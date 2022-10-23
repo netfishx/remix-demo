69d107331377b48013e67cd3394174a22c1134e8
wangwupeng <netfishx@gmail.com>
2022-10-23 21:20:43 +0800

    add unocss

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig, presetUno } from 'unocss'
+import { defineConfig, presetUno, presetAttributify } from 'unocss'
 
 export default defineConfig({
-  presets: [presetUno()]
+  presets: [presetUno(), presetAttributify()]
 });
